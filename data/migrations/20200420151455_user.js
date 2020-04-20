687f6eb4967ee156d892f092dea2ac91de584284
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-20 15:18:56 -0600

    user migration made, user table schema

---
+++ data/migrations/20200420151455_user.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('user', tbl => {
+        tbl.increments();
+        tbl.string('username')
+            .notNullable()
+            .unique();
+        tbl.string('password')
+            .notNullable();
+        tbl.string('department')
+            .notNullable();
+  })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('user')
+};
