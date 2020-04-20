ea8a9cb4b03d47952fa70f6db4205d8c6604619e
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-20 15:12:57 -0600

    knexfile and setup within

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/na2p.db3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
+    migrations: {
+      directory: './data/migrations'
+    }
+  }
+}
